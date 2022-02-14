cfd76173b77039666529db4a181a4181d6c8bd34
david-aihe <doa9200@gmail.com>
2022-02-14 12:05:43 -0500

    Added middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,7 +1,6 @@
+const AccountsModel = require('./accounts-model')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
   const {name, budget} = req.body;
   if(!name || !name.trim() || !budget || !Number(budget)){
     res.status(400).json({message: "name and budget are required"})
@@ -22,9 +21,31 @@ exports.checkAccountPayload = (req, res, next) => {
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  const {name} = req.body
+  const accounts = AccountsModel.getAll();
+
+  const found = accounts.filter(account=> account.name === name.trim())
+  
+  if(found){
+    res.status(400).json({message: "that name is taken"})
+  }
+  else{
+    req.name = name.trim()
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try{
+    const account = await AccountsModel.getById(req.params.id)
+    if(!account){
+      res.status(404).json({message: 'account not found'})
+    }
+    else{
+      req.account = account;
+      next()
+    }
+  }
+  catch (err){
+    res.status(500).json({message: 'problem finding account'})
+  }
 }
