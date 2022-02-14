48fa7cf70899460fa1696e58a3327e6089c59650
david-aihe <doa9200@gmail.com>
2022-02-14 11:43:19 -0500

    checkAccountPayload middleware added

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,6 +2,23 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  const {name, budget} = req.body;
+  if(!name || !name.trim() || !budget || !Number(budget)){
+    res.status(400).json({message: "name and budget are required"})
+  }
+  else if(name.trim().length < 3 || name.trim().length > 100){
+    res.status(400).json({message: "name of account must be between 3 and 100"})
+  }
+  else if(Number(budget).isNaN()){
+    res.status(400).json({message: "budget of account must be a number"})
+  }
+  else if(budget < 0 || budget > 1000000){
+    res.status(400).json({message: "budget of account is too large or too small"})
+  }
+  else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
