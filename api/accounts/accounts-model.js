d284b4e661dfa031e07518be3b0ceff4cb3f6991
david-aihe <doa9200@gmail.com>
2022-02-14 11:16:56 -0500

    Model functions created

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,26 @@
 const getAll = () => {
   // DO YOUR MAGIC
+  db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  db('accounts').where('id',id);
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  db('accounts').insert(account);
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  db('accounts').where('id',id).update(account);
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  db('accounts').where('id',id).delete();
 }
 
 module.exports = {
